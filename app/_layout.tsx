2fe36656bc09f946057d912b2295c94f696b074f
Michael Rechberger <71632874+rechbe200163@users.noreply.github.com>
2025-08-09 15:37:11 +0200

    chore: add Nativewind and Tailwind CSS configuration
    - Added Nativewind as a dependency in package.json.
    - Included Tailwind CSS as a dependency and added its configuration file (tailwind.config.js).
    - Updated TypeScript configuration to include nativewind-env.d.ts for type definitions.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import '../global.css';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function RootLayout() {
@@ -20,10 +24,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack.Screen name='+not-found' />
       </Stack>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </ThemeProvider>
   );
 }
